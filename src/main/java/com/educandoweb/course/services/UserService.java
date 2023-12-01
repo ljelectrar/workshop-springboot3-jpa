d87c17a555fa434c712d6004f7255b14c2e6f636
Leandro Junior Alves Dos Santos <ljelectrar@outlook.com>
2023-12-01 12:05:40 -0300

    Insert user.

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -17,13 +17,15 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return userRepository.findAll();
-		
 	}
 	
 	public User findById(Long id) {
 		
 		Optional<User> obj = userRepository.findById(id);
 		return obj.get();
-		
+	}
+	
+	public User insert(User obj) {
+		return userRepository.save(obj);
 	}
 }
