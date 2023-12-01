672d621032871b3fd87636d14e1335166768a614
Leandro Junior Alves Dos Santos <ljelectrar@outlook.com>
2023-12-01 11:43:36 -0300

    Subtotal & Total methods.

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -107,6 +107,13 @@ public class Order implements Serializable {
 		return items;
 	}
 
+	public Double getTotal() {
+		double sum = 0;
+		for (OrderItem x : items) {
+			sum += x.getSubtotal();
+		}
+		return sum;
+	}
 	
 	@Override
 	public int hashCode() {
